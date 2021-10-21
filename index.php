1b06a97fd2463f6c898856a9f097ab0bd61411d4
SpeekeR <DominikZappe1999@seznam.cz>
2021-10-21 16:30:44 +0200

    Added index.php and style.css None of these files do anything useful now

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+?>
+<!doctype html>
+<html lang="cs">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Zappe_SP_WEB</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <header>
+            <h1>Hlavička</h1>
+        </header>
+    <article>
+        <div>Něco</div>
+    </article>
+        <footer>
+            (c) 2021
+        </footer>
+    </body>
+</html>
