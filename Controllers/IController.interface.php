a4c8c973d656d674926a4118614f3d0021a3586f
SpeekeR <DominikZappe1999@seznam.cz>
2021-11-06 20:43:05 +0100

    IController, interface for generic controller

--- Controllers/IController.interface.php
+++ Controllers/IController.interface.php
@@ -1,1 +1,18 @@
 <?php
+
+/**
+ * Rozhrani generickeho ovladace
+ */
+interface IController {
+
+    /**
+     * Metoda show musi zajistit spravne vykresleni dane stranky
+     *
+     * @param string $pageTitle Nazev stranky
+     * @return string HTML stranky
+     */
+    public function show(string $pageTitle):string;
+
+}
+
+?>
