c3443b2a8cecf79b7911c19a39b3f6a0b07c81fc
SpeekeR <DominikZappe1999@seznam.cz>
2021-11-06 20:59:42 +0100

    TemplateHTML class added for getting header and footer

---
+++ Views/TemplateHTML.class.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * Trida pro vytvoreni hlavicky a paticky HTML
+ */
+class TemplateHTML {
+
+    /**
+     * Pripojeni CSS
+     */
+    public function __construct() {
+        require_once("style.css");
+    }
+
+    /**
+     * Vytvori hlavicku HTML stranky, az po obsah
+     * @param string $pageTitle Nazev stranky
+     */
+    public function getHTMLHeader(string $pageTitle) {
+        ?>
+        <!doctype html>
+        <html lang="cs">
+        <head>
+            <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title><?php echo $pageTitle; ?></title>
+            <link rel="stylesheet" href="style.css">
+        </head>
+        <body>
+            <h1><?php echo $pageTitle; ?></h1>
+            <nav>
+                <?php
+                    foreach(WEB_PAGES as $key => $pInfo){
+                        echo "<a href='index.php?page=$key'>$pInfo[title]</a>";
+                    }
+                ?>
+            </nav>
+            <br>
+<?php
+    }
+
+    /**
+     * Vytvori paticku HTML, od obsahu dal
+     */
+    public function getHTMLFooter() {
+        ?>
+            <br>
+            <footer>
+                &copy; 2021
+            </footer>
+        </body>
+        </html>
+<?php
+    }
+
+}
+
+?>
--- index.php
+++ index.php
@@ -3,26 +3,7 @@
 require_once("settings.inc.php");
 require_once("appStart.class.php");
 
+$app =new AppStart();
+$app->run();
+
 ?>
-
-
-<!doctype html>
-<html lang="cs">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Zappe_SP_WEB</title>
-        <link rel="stylesheet" href="style.css">
-    </head>
-    <body>
-        <header>
-            <h1>Hlavička</h1>
-        </header>
-    <article>
-        <div>Něco</div>
-    </article>
-        <footer>
-            &copy; 2021
-        </footer>
-    </body>
-</html>
